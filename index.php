d7123673bf04cff5f241f787737effd1023c5952
Logan Butler <logan@terum.us>
2015-06-15 22:21:56 -0400

    Added
    Added porject "spiders". These motherfuckers wont leave your mouse
    cursor alone while you're at the webpage they're on.

---
+++ index.php
@@ -0,0 +1,36 @@
+
+
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+    <title>NEXUS - Terum.us-</title>
+    <meta name="y_key" content="4565d9e117592cfa" />
+    <link href='http://fonts.googleapis.com/css?family=Nova+Square' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link href="css/imports.min.css" rel="stylesheet">
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css" />
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"> </script>
+	<script type="text/javascript" src="main.one.js"> </script>
+   </head>
+  <body>
+<!-- Intro Header -->
+<header class="intro" >
+    <canvas id="spiders" class="hidden-xs"></canvas>
+    <div class="intro-body">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+				<h1>aweh yeah.</h1> <br /> fucking spiders.. they're everywhere.
+				</div>
+            </div>
+        </div>
+    </div>
+</header>
+
+</body>
+</html>
